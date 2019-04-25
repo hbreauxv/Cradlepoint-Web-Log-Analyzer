c7b531e0f890d931f6503948ea1631ed5e446a1a
AlexInskeep <ainskeep@cradlepoint.com>
2019-04-25 08:31:53 -0600

    Adding redirects for file upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, url_for
 from bokeh.plotting import figure
 from bokeh.embed import components
 from forms import logFileForm
@@ -7,15 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '\x7f[\xce\x97\xf9\x86\x1b\x92YBx/7\xdcX^\xea\xd5\xc4\t~\x8c\xbe\x02'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def showDashboard():
 	form = logFileForm()
-
-	if form.validate_on_submit():
-		logFileName = form.logFile.data.filename
-		form.logFile.data.save('logFiles/' + logFileName)
-		flash("LogFile: {} has been submitted".format(logFileName))
-
 
 	plots = []
 	plots.append(makePlot())
@@ -23,7 +17,16 @@ def showDashboard():
 	return render_template('dashboard.html', plots=plots, form=form)
 
 
+@app.route('/UploadFile', methods=['POST'])
+def uploadFile():
+	form = logFileForm()
 
+	if form.validate_on_submit():
+		logFileName = form.logFile.data.filename
+		form.logFile.data.save('logFiles/' + logFileName)
+		flash("LogFile: {} has been submitted".format(logFileName))
+
+	return redirect(url_for('showDashboard'))
 
 def makePlot():
 	plot = figure(plot_height=150, sizing_mode='scale_width')
