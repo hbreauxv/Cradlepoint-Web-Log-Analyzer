b26f93dc5131993f81273dd0976e29f8d288be4b
jzabriskie <jzabriskie@cradlepoint.com>
2019-04-25 10:17:36 -0600

    ConnStateParse uses logparser, integraded with web app

--- ConnStateParse.py
+++ ConnStateParse.py
@@ -7,6 +7,7 @@ from copy import copy
 from datetime import datetime
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import ColumnDataSource, HoverTool
+from web_log_parser import logFile
 
 class ConnStateParse():
     #Wan state enumeration
@@ -56,6 +57,7 @@ class ConnStateParse():
     #Example lines. Parse out time, uid, last state, new state, reason (if given)
     #2019-04-19 03:37:05 192.168.0.1 S= INFO ﻿WAN:685ca069 -- connecting -> disconnecting
     #2019-04-19 03:35:51 192.168.0.1 S= INFO ﻿WAN:686be2ac -- connecting -> connected, Reason: Failback
+    @classmethod
     def _parseDevState(self, line):
         retEvt = None
         rgxDevState = r'^(\d*-\d*-\d* \d*:\d*:\d*).*WAN:(.*) -- (?!Service Change)(.*) -> (.*?)(?:, Reason: (.*))?$'
@@ -72,6 +74,7 @@ class ConnStateParse():
             retEvt = self.WanEvent(time,uid,state,details)
         return retEvt
 
+    @classmethod
     def _parseUnplug(self, line):
         retEvt = None
         rgxUnplug = r'^(\d*-\d*-\d* \d*:\d*:\d*).*WAN:(.*) -- Unplugged$'
@@ -82,7 +85,8 @@ class ConnStateParse():
             state = "unplugged"
             retEvt = self.WanEvent(time,uid,state)
         return retEvt
-    
+
+    @classmethod
     def _parsePlug(self, line):
         retEvt = None
         rgxPlug = r'^(\d*-\d*-\d* \d*:\d*:\d*).*WAN:(.*) -- Plug event: ok$'
@@ -94,6 +98,7 @@ class ConnStateParse():
             retEvt = self.WanEvent(time,uid,state)
         return retEvt
     
+    @classmethod
     def _parseConfigure(self, line):
         retEvt = None
         rgxConfig = r'^(\d*-\d*-\d* \d*:\d*:\d*).*WAN:(.*) -- Configure Event:.*$'
@@ -106,36 +111,42 @@ class ConnStateParse():
         return retEvt
 
     #Main parsing funcion
-    #Given file name, open file, parse it
+    #Given file name parse it and return the specified format
     #Output is {uid:[[time,state,details],],}
     #output optional CSV string of all data
-    def parseLog(self, filename, fileLines=None, csv=False):
+    @classmethod
+    def parseLog(self, filename, retType):
+        retTypes = ['dict', 'csv', 'plot']
+        if retType not in retTypes:
+            raise ValueError(' retType must be in {}'.format(retTypes))
         #Every function in list below will be executed on every line
         parseFuncs = [self._parseDevState, self._parseUnplug, self._parsePlug] # Every function here should parse a line and return a wanEvt
-        if fileLines:
-            lines = fileLines
-        else:
-            with open(filename, 'r') as f:
-                lines = f.readlines()
+        log = logFile(filename)
+        log.open()
         retCSV = self.WanEvent.getCSVHeader()  # Start building string of all parsed data in CSV output format
         retDict = {}
-        for line in lines:
+        for line in log:
             for func in parseFuncs:
                 evt = func(line)
                 if evt:  # If evt not None, we've got a new event to add
-                    #Building CSV output
-                    retCSV += evt.getCSV()
-                    #Here we're building dictionary output
-                    if not evt.uid in retDict:
-                        retDict[evt.uid] = []
-                    retDict[evt.uid].append(evt.getList())
+                    if retType == 'csv':  # Building CSV output
+                        retCSV += evt.getCSV()
+                    if retType in ['dict', 'plot']:  # Here we're building dictionary output
+                        if not evt.uid in retDict:
+                            retDict[evt.uid] = []
+                        retDict[evt.uid].append(evt.getList())
                     break
+        log.close()
         # Return format
-        if csv:
+        if retType == 'csv':
             return retCSV
-        return retDict
-    
-    def showBokehGraph(self, graphDict):
+        if retType == 'dict':
+            return retDict
+        if retType == 'plot':
+            return self.getPlot(retDict)
+        
+    @classmethod
+    def getPlot(self, graphDict, view=False):
         #Function showing an example interpretation of the parseLog functions
         #Features here: Step graph (using mode 'after'), circles on points for better visuals,
         #   Tooltips showing desc data, legend with 'hide' option, y_range using strings
@@ -144,10 +155,10 @@ class ConnStateParse():
             ("Details", "@desc"),
         ]
         connStateRange=[x.name for x in ConnStateParse.WANState] #Get list of state strings
+        #Colors here are what will be used to color lines (in order) TODO make sure that we just loop if we hit the end
         colors = iter(['red','blue','green','deepskyblue', 'navy', 'rosybrown', 'darkgoldenrod', ' aquamarine', 'olive', 'orangered', 'orange', 'pink', 'purple', 'indigo',])
         p=figure(plot_width=1000, x_axis_type='datetime', y_range=connStateRange, tooltips=TOOLTIPS)
         p.title.text = 'Connection State Graph'
-
         for uid in graphDict:
             color = next(colors)
             datetimes = [elt[0] for elt in graphDict[uid]]
@@ -164,7 +175,9 @@ class ConnStateParse():
         p.legend.location = "bottom_center"
         p.legend.orientation = "horizontal"
         p.legend.click_policy="hide"
-        show(p)
+        if view:
+            show(p)
+        return p        
 
 
 
@@ -176,8 +189,10 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     filename = args.filename
-    hp = ConnStateParse()
-    resl = hp.parseLog(filename)  # Here we get our standard output. {uid1:[[event1,...eventN],[...]],...uidN:[[]]}
-    hp.showBokehGraph(resl)  # This function may be moved outside of this class. Doesn't depend upon class vars. Simpy
+
+    #Example usage of the class
+    resl = ConnStateParse.parseLog(filename, 'dict')  # Here we get our standard output. {uid1:[[event1,...eventN],[...]],...uidN:[[]]}
+    ConnStateParse.getPlot(resl, view=True)  # Plotting and viewing
+    #ConnStateParse.parseLog(filename, 'log')  # Getting a plot back, to be viewed, saved, embedded
     
 
