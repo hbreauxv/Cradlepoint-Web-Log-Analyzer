797c0be9aa0cb82e508ae5e583d569aa3d0c11c1
Harvey Breaux <hbreauxv@gmail.com>
2019-11-27 15:44:15 -0700

    added Log Analysis feature

--- app.py
+++ app.py
@@ -5,69 +5,94 @@ from forms import logFileForm
 from ConnStateParse import ConnStateParse
 from LogFile import logFile
 from SignalQualityParser import signalQualityParser
+from scan_log import ScanLog
 from os import remove
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\x7f[\xce\x97\xf9\x86\x1b\x92YBx/7\xdcX^\xea\xd5\xc4\t~\x8c\xbe\x02'
+
+scanner = ScanLog(None, None, log_database='./log_messages.json')
 
 
 ## View functions
 @app.route('/')
 def showDashboard():
-	form = logFileForm()
+    form = logFileForm()
 
-	plots = []
-	fileNameLoc = session.pop('logFileLoc', None)
-	if fileNameLoc:
-		plots = generatePlots(fileNameLoc)
+    plots = []
+    analysis = ''
+    fileNameLoc = session.pop('logFileLoc', None)
+    if fileNameLoc:
+        plots = generatePlots(fileNameLoc)
+        analysis = search_log(fileNameLoc)
+        remove(fileNameLoc)
 
-	return render_template('dashboard.html', plots=plots, form=form)
+    return render_template('dashboard.html', plots=plots, form=form, analysis=analysis)
+
 
 @app.route('/UploadFile', methods=['POST'])
 def uploadFile():
-	form = logFileForm()
+    form = logFileForm()
 
-	if form.validate_on_submit():
-		logFileName = form.logFile.data.filename
-		savedLocation = 'logFiles/' + logFileName
-		form.logFile.data.save(savedLocation)
-		flash("LogFile: {} has been submitted".format(logFileName))
+    if form.validate_on_submit():
+        logFileName = form.logFile.data.filename
+        savedLocation = 'logFiles/' + logFileName
+        form.logFile.data.save(savedLocation)
+        flash("LogFile: {} has been submitted".format(logFileName))
 
-		session.pop('logFileLoc', None) #clear old logFileLoc from session
-		session['logFileLoc'] = savedLocation
+        session.pop('logFileLoc', None)  # clear old logFileLoc from session
+        session['logFileLoc'] = savedLocation
 
-	return redirect(url_for('showDashboard'))
+    return redirect(url_for('showDashboard'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def server_error(e):
-	return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 
 def generatePlots(logFileLoc):
+    plots = []
+    try:
+        log = logFile(logFileLoc)
+        log.open()
+    except FileNotFoundError as e:
+        print('Could not find file: {}'.format(e))
+        return []
 
-	plots = []
-	try:
-		log = logFile(logFileLoc)
-		log.open()
-	except FileNotFoundError as e:
-		print('Could not find file: {}'.format(e))
-		return []
+    connStatePlot = ConnStateParse.parseLog(log, 'plot')
+    plots.append(components(connStatePlot))
 
-	connStatePlot = ConnStateParse.parseLog(log, 'plot')
-	plots.append(components(connStatePlot))
+    sigQParse = signalQualityParser()
+    sigQPlot = sigQParse.parseLog(log, 'plot')
 
-	sigQParse = signalQualityParser()
-	sigQPlot = sigQParse.parseLog(log, 'plot')
+    for figure in sigQPlot:
+        plots.append(components(figure))
 
-	for figure in sigQPlot:
-		plots.append(components(figure))
 
-	# this line breaks things on windows
-	# remove(logFileLoc)  # don't want these files to build up, remove after parse
+    # this line breaks things on windows
+    # remove(logFileLoc)  # don't want these files to build up, remove after parse
 
-	return plots
+    return plots
+
+
+def search_log(logFileLoc):
+    """Search Log file for problematic messages"""
+    problem_messages = []
+
+    try:
+        log = logFile(logFileLoc)
+        log.open()
+    except FileNotFoundError as e:
+        print('Could not find file: {}'.format(e))
+        return []
+
+    scanner.input_file = logFileLoc
+    problem_messages = scanner.search_log()
+
+    return problem_messages
--- forms.py
+++ forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
 
+
 class logFileForm(FlaskForm):
-	logFile = FileField(validators=[FileRequired()])
+    logFile = FileField(validators=[FileRequired()])
+
